3c28072a4efaa12d4ddce11c945a65d47bb423d3
jemish-169 <jemishkhunt169@gmail.com>
2022-12-05 23:38:08 +0530

    no message

---
+++ 1742A.cpp
@@ -0,0 +1,30 @@
+/*
+Sum
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    cin >> cases;
+    while (cases--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b == c || b + c == a || c + a == b)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
