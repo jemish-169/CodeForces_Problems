97f70d0790694e6c2b626518004da983a4bbbb80
jemish-169 <jemishkhunt169@gmail.com>
2023-01-04 23:49:30 +0530

    no message

---
+++ 1553A.cpp
@@ -0,0 +1,29 @@
+/*
+Digit sum
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    cin >> cases;
+    while (cases--)
+    {
+        int x, ans(0);
+        cin >> x;
+        if (x % 10 == 9)
+            ans++;
+        cout << x / 10 + ans << endl;
+    }
+    return 0;
+}
