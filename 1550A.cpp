8fb2fa7153f8b29577e89d48e4d7b52ea44cc00b
jemish-169 <jemishkhunt169@gmail.com>
2023-01-10 23:31:39 +0530

    no message

---
+++ 1550A.cpp
@@ -0,0 +1,30 @@
+/*
+find the array
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    cin >> cases;
+    while (cases--)
+    {
+        int x, res = 1;
+        cin >> x;
+        while (pow(res, 2) < x)
+            res++;
+        cout << res << endl;
+    }
+    return 0;
+}
