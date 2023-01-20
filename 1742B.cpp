57668e0d20a02a401d693918c371eeeb00ad2ea0
jemish-169 <jemishkhunt169@gmail.com>
2023-01-20 18:21:33 +0530

    no message

---
+++ 1742B.cpp
@@ -0,0 +1,32 @@
+/*
+Increasing
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    cin >> cases;
+    while (cases--)
+    {
+        unordered_set<int> st;
+        int x, t;
+        cin >> x;
+        for (int i = 0; i < x; i++)
+        {
+            cin >> t;
+            st.insert(t);
+        }
+        cout << (st.size() == x ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
