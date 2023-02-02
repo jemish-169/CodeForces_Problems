36437287d46572a6f3d88dfff729685aa0d4c979
jemish-169 <jemishkhunt169@gmail.com>
2023-02-03 00:00:14 +0530

    no message

---
+++ 1772A.cpp
@@ -0,0 +1,27 @@
+/*
+A+B
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    cin >> cases;
+    while (cases--)
+    {
+        char a, b, c;
+        cin >> a >> b >> c;
+        cout << a - '0' + c - '0' << endl;
+    }
+    return 0;
+}
