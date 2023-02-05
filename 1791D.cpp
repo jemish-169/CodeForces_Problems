73c33ad472d9a4b4dd0849fcb8a335c0a9f52858
jemish-169 <jemishkhunt169@gmail.com>
2023-02-05 16:03:58 +0530

    no message

--- 1791D.cpp
+++ 1791D.cpp
@@ -2,12 +2,10 @@
 Distinct Split
 */
 #include <bits/stdc++.h>
-#define int int64_t
 using namespace std;
 map<int, int> mp;
 list<int> ls;
 vector<int> vec;
-int maxi = INT_MIN;
 int mini = INT_MAX;
 int32_t main()
 {
@@ -18,18 +16,22 @@ int32_t main()
     cin >> cases;
     while (cases--)
     {
-        int x;
+        int n, ans(0);
         string s;
-        cin >> x >> s;
-        set<char> st, str;
+        cin >> n >> s;
+        vector<int> cnt(26, 0), p(26, 0);
+        for (auto i : s)
+            ++cnt[i - 'a'];
         for (auto i : s)
         {
-            if (st.find(i) == st.end())
-                st.insert(i);
-            else
-                str.insert(i);
+            --cnt[i - 'a'];
+            ++p[i - 'a'];
+            int cur = 0;
+            for (int i = 0; i < 26; i++)
+                cur += min(1, cnt[i]) + min(1, p[i]);
+            ans = max(ans, cur);
         }
-        cout << st.size() + str.size() << endl;
+        cout << ans << endl;
     }
     return 0;
 }
