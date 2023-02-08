c837dc0cd205f1189dce762e8b74ee01c2bd3e2d
jemish-169 <jemishkhunt169@gmail.com>
2023-02-08 23:59:44 +0530

    no message

---
+++ 253A.cpp
@@ -0,0 +1,37 @@
+/*
+Boys and Girls
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    // cin >> cases;
+    while (cases--)
+    {
+        int a, b;
+        cin >> b >> a;
+        while (a > 0 && b > 0)
+        {
+            if (b--)
+                cout << "B";
+            if (a--)
+                cout << "G";
+        }
+        while (--b >= 0)
+            cout << "B";
+        while (--a >= 0)
+            cout << "G";
+    }
+    return 0;
+}
