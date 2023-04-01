f3d31a9ed0103afe7f768d63329224fd2a75b79a
jemish-169 <jemishkhunt169@gmail.com>
2023-04-01 23:59:30 +0530

    no message

---
+++ 1810A.cpp
@@ -0,0 +1,37 @@
+/*
+Beautiful sequence
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    cin >> cases;
+    while (cases--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 1; i <= n; i++)
+            cin >> a[i];
+        for (int i = 1; i <= n; i++)
+        {
+            if (a[i] <= i)
+            {
+                cout << ("YES");
+                break;
+            }
+        }
+        cout << ("NO");
+    }
+    return 0;
+}
