5ef29b8de24f8b8bcd5c799da1436cd3c4e0f6f7
jemish-169 <jemishkhunt169@gmail.com>
2023-04-10 23:54:12 +0530

    no message

---
+++ 1811B.cpp
@@ -0,0 +1,24 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    cin >> cases;
+    while (cases--)
+    {
+        }
+    return 0;
+}
